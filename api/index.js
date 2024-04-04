ed83163402d4f0c0703bb3000eb2d14b439e105a
SushantShelake <124454809+sushant2209@users.noreply.github.com>
2024-04-05 02:02:47 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import fs from "fs";
+import cors from "cors";
+
+const port = 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+const path1 = "./weights.json";
+var weightData = JSON.parse(fs.readFileSync(path1, "utf-8"));
+
+app.get("/get_array", (req, res) => {
+  res.json(weightData);
+});
+
+app.put("/update_weight", (req, res) => {
+  const newWeight = req.body.weight;
+  for (let i = 2; i < 30; i++) {
+    weightData[i - 1] = weightData[i];
+  }
+  weightData[29] = newWeight;
+  fs.writeFileSync(path1, JSON.stringify(weightData));
+  res.json({ message: "Weight updated successfully" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
